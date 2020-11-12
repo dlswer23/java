19ac83eabf16e46f93b47f494325244f94170a37
dlswer23 <201114leb@dsm.hs.kr>
2020-11-12 10:16:31 +0900

    [ADD]자기소개만들기

---
+++ intoduce.java
@@ -0,0 +1,16 @@
+public class intoduce{
+
+        public static void main(String[] args) {
+            String name = "코드잇";
+            int year = 2015;
+            int month = 7;
+            int day = 20;
+            boolean married = false;
+
+            System.out.println("이름: " + name);
+            System.out.println("생일: " + year+ "년 " + month + "월 " + day + "일");
+            System.out.println("결혼: " + married);
+        }
+    }
+
+
