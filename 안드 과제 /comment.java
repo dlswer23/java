d85baf1401b8366e2e34a90575dab1463348c288
dlswer23 <201114leb@dsm.hs.kr>
2020-11-22 19:22:57 +0900

    [REMOVE]잘못된 파일 삭제

--- 안드 과제 /comment.java
+++
@@ -1,8 +0,0 @@
-public class comment {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World!");
-        System.out.println("Hi, Java!");
-        //System.out.println("Bye, Java!");
-    }
-}
