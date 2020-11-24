881e91570936ef2263e46e232215c80c9cb42b9a
dlswer23 <201114leb@dsm.hs.kr>
2020-11-24 22:49:03 +0900

    [ADD]02-2. 변수와 타입 그리고 연산자
    예제 코드 첨부

---
+++ 안드 과제 /2020.11.24/Lab02Review.java
@@ -0,0 +1,16 @@
+public class Lab02Review {
+
+    public static void main(String[]args){
+
+        String name ="홍팍";
+        int age = 34;
+        String isMale ="true";
+
+        System.out.println("이름 " + name);
+        System.out.println("나이 " + age);
+        System.out.println("성별 " + isMale);
+
+
+
+    }
+}
