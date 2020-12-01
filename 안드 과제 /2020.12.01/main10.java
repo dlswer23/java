c214d0e70e9ed151ad6cc56e3c74eec373a11614
dlswer23 <201114leb@dsm.hs.kr>
2020-12-01 19:28:29 +0900

    [ADD] 코드잇 #10

---
+++ 안드 과제 /2020.12.01/main10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main10 {
+
+    public static void main(String[]args){
+
+        int a;
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+        System.out.printf("%d",a);
+    }
+}
