e2310a895f1d1906d90e93b39ad640da668e72fb
dlswer23 <201114leb@dsm.hs.kr>
2020-12-01 19:28:37 +0900

    [ADD] 코드잇 #11

---
+++ 안드 과제 /2020.12.01/main11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main11 {
+
+
+    public static void main(String []args){
+
+        Scanner scan = new Scanner(System.in);
+        float a = scan.nextFloat();
+        System.out.printf("%f" ,a);
+    }
+
+}
