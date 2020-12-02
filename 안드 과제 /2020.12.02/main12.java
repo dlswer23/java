c434dc199877263d2c2ea8e0b5f4f2c46d3f4be7
dlswer23 <201114leb@dsm.hs.kr>
2020-12-02 12:06:51 +0900

    [ADD] 코드잇 #12

---
+++ 안드 과제 /2020.12.02/main12.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main12 {
+
+    public static void main(String []args){
+
+        String a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.next();
+
+        System.out.println(a);
+    }
+}
+
