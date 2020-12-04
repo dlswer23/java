b9eb98dd42953b4ecab1b85505f8e2150118737d
dlswer23 <201114leb@dsm.hs.kr>
2020-12-04 22:35:46 +0900

    [ADD] 코드잇 #15

---
+++ 안드 과제 /2020.12.04/main15.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class main15 {
+
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        System.out.println(String.format("%.2f",a));
+
+
+
+
+    }
+}
