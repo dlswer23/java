6689c86a0d134a99f4d3d735301e0afee1b65325
dlswer23 <201114leb@dsm.hs.kr>
2020-12-04 22:35:30 +0900

    [ADD] 코드잇 #14

---
+++ 안드 과제 /2020.12.04/main14.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class main14 {
+
+    public static void main(String []args){
+        Scanner sc= new Scanner(System.in);
+
+        String s= sc.nextLine();
+
+         char x=s.charAt(0);
+        char y=s.charAt(2);
+
+        System.out.println(y+ " "+x);
+
+
+    }
+}
