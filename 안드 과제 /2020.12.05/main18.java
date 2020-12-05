c1a2ea78d555626768725308f70c5e7e69b5b4fe
dlswer23 <201114leb@dsm.hs.kr>
2020-12-05 23:19:55 +0900

    [ADD] 코드잇 #18

---
+++ 안드 과제 /2020.12.05/main18.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class main18{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String time = scan.nextLine();
+        System.out.print(time);
+    }
+}
