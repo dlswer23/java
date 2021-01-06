16333468f4362e931cc81dc53e45fd589bec352f
dlswer23 <lbj4298@naver.com>
2021-01-06 21:32:09 +0900

    [ADD] if문 사용하기

---
+++ 안드 과제 /2020.1.6/if문.java
@@ -0,0 +1,16 @@
+public class if문 {
+
+    public static void main(){
+        int a=5;
+
+        if(a >= 9){
+            System.out.println("1");
+        }
+        else if(a >= 7){
+            System.out.println("2");
+        }
+        else if(a >=5 ){
+            System.out.println("3");
+        }
+    }
+}
