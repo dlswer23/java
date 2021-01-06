ff963259d6e32346c91df21e674e4d245a524755
dlswer23 <lbj4298@naver.com>
2021-01-06 20:48:27 +0900

    [ADD]1차원 for문

---
+++ 안드 과제 /2020.1.6/for문.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class for문 {
+
+    public static void main(String args[]){
+        int a;
+        int b=10;
+       // Scanner scan = new Scanner(System.in);
+
+        System.out.println("숫자를 입력하시오");
+        for(a=0;a<b;a++){
+
+            System.out.println("*");
+        }
+        System.out.println("");
+    }
+}
