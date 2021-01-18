b639ac4bf6d8b96aedfe9b72c4b4f280385c1241
dlswer23 <lbj4298@naver.com>
2021-01-18 20:32:04 +0900

    [ADD] 객체 사용하기

---
+++ TIL/2021.1.18/method.java
@@ -0,0 +1,17 @@
+public class method {
+
+    String n;
+    int a;
+    void run(){
+    }
+
+    static void increase(int m){
+        m++;
+    }
+    public static void main(String []args){
+        int n = 2;
+        increase(2);
+        System.out.println();
+    }
+}
+
