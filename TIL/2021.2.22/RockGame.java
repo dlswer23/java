9b9cee80683d2426192478317cd47a7378f0a5b6
dlswer23 <lbj4298@naver.com>
2021-02-23 00:05:04 +0900

    [ADD]2021.2.22

---
+++ TIL/2021.2.22/RockGame.java
@@ -0,0 +1,56 @@
+/*package week20;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockGame {
+
+    String str;
+
+    void run() {
+
+        System.out.println("컴퓨터와 가위 바위 보 게임을 합니다");
+        System.out.print("가위 바위 보! >> ");
+
+        str = sc.nextLine();
+
+    }
+
+     static void result(){
+
+
+
+        String [] a = {"주먹", "가위", "보"};
+
+
+        String rr;
+
+
+
+        for(int i : a) {
+
+            rr = (String) (Math.random());
+
+            System.out.println(rr);
+
+        }
+
+    }
+
+    Scanner sc = new Scanner(System.in);
+    static Random rd = new Random();
+
+    public static void main(String args[]) {
+        RockGame rg = new RockGame();
+        rg.run();
+
+
+
+    }
+}
+*/
+
+
+
+
