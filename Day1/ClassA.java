1f2f5c6a7d80e3c48a73ead80a9ddd4b4b0d1c43
이은별 <lbj4298@naver.com>
2021-03-09 14:44:59 +0900

    [ADD] Class 1

---
+++ Day1/ClassA.java
@@ -0,0 +1,23 @@
+package Day1;
+import java.util.Scanner;
+
+public class ClassA {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+	
+		
+		String n = sc.next();
+		
+		int a = sc.nextInt();
+
+		System.out.println(n);
+
+		System.out.println(a);
+
+	}
+
+}
---
+++ Day1/practice.java
@@ -0,0 +1,18 @@
+package Day1;
+
+public class practice {
+
+	//Á¤Àû static 
+	static int a;
+	
+	public static void main(String []args) {
+		
+		int b=1;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		
+		
+	}
+}
