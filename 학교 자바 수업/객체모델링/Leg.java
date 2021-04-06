6a5c8d5932cf16f95c3c24575fbcb50b702e681d
dlswer23 <lbj4298@naver.com>
2021-04-06 23:09:36 +0900

    [ADD] 객체 모델링

---
+++ 학교 자바 수업/객체모델링/Leg.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Leg {
+
+    int pattern;
+    int length;
+
+    Leg(int pattern,int length){
+
+        this.pattern = 33;
+        this.length = 22;
+    }
+
+    public void final1(){
+
+        System.out.println("로봇의 다리의 문양의 개수는 " + pattern+"입니다");
+        System.out.println("로봇의 다리 길이  "+length + "입니다");
+        System.out.println("");
+
+    }
+
+}
