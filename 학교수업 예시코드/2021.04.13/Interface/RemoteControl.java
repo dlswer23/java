4d61b9f07e33ddd671dd7e0ef4c394b1ba7c6be2
이은별 <lbj4298@naver.com>
2021-04-13 16:09:57 +0900

    [ADD] Interface Example Code

---
+++ 학교수업 예시코드/2021.04.13/Interface/RemoteControl.java
@@ -0,0 +1,30 @@
+package Interface;
+
+public interface RemoteControl {
+	
+	//상수 
+	public static final int MAX_VOLUME = 10;
+	public static final int MIN_VOLUME = 0;
+	
+	//추상메소드
+	
+	//@Override
+	void turnOn();
+	void turnOff();
+	void setVolume(int volume);
+	
+	//디폴트 메소드 
+	default void setMute(boolean mute) {
+		if(mute) {
+			System.out.println("무음 처리합니다");
+		}
+		else {
+			System.out.println("무음 해제합니다");
+		}
+	}
+	
+	//정적 메소드 
+	static void changeBattery() {
+		System.out.println("건전지를 교환합니다");
+	}
+}
---
+++ 학교수업 예시코드/2021.04.13/Interface/RemoteControlExample.java
@@ -0,0 +1,26 @@
+package Interface;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc = new RemoteControl() {
+
+			@Override
+			public void turnOn() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void turnOff() {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void setVolume(int volume) {
+				// TODO Auto-generated method stub
+
+			}
+		};
+	}
+}
